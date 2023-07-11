70f3bfcda072bb8be6b41a867a557cd0caec839c
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2023-07-10 21:53:57 -0300

    Initial Commit

---
+++ src/test/java/com/medsoft/labmedial/LabMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medsoft.labmedial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
